8ea257f430baca9f871ea9947295e37fb3fd19b0
Vladislav ZHereb'ev <111141693+redrepublics@users.noreply.github.com>
2026-08-26 13:36:46 +0300

    Playwrigtht test run

---
+++ lesson.py
@@ -0,0 +1,25 @@
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://demo.playwright.dev/todomvc/#/")
+    page.get_by_role("link", name="real TodoMVC app.").click()
+    with page.expect_download() as download_info:
+        page.get_by_role("link", name="Download").click()
+    download = download_info.value
+    page.get_by_role("link", name="React New", exact=True).click()
+    page.get_by_test_id("text-input").click()
+    page.get_by_test_id("text-input").fill("hi")
+    page.get_by_test_id("text-input").press("Enter")
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
